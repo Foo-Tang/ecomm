d71d6822542b54944860b8b5bcb825b41adfb761
Chelsea Bahsler <chelseabahsler@gmail.com>
2023-11-28 15:03:01 -0600

    Redirect to checkout works

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,9 +1,5 @@
 class CheckoutController < ApplicationController
   def create
-    # Load up the product the user wishes to purchase from the product model:
-
-    # product = Product.find(params[:product_id])
-
     # # DONT EVEN BOTHER CONTINUING IF SOMEONE IS MESSING WITH US
     # if product.nil?
     #   redirect_to root_path
@@ -17,26 +13,25 @@ class CheckoutController < ApplicationController
         redirect_to root_path
         return
       end
-      items <<{
+      items << {
         price_data: {
-          unit_amount: (product.price * 100).to_i,
-          currency: 'cad',
+          unit_amount:  (product.price * 100).to_i,
+          currency:     "cad",
           product_data: {
-            name: product.name,
-            description: product.description,
-          },
+            name:        product.name,
+            description: product.description
+          }
         },
-        quantity: 1
+        quantity:   1
       }
     end
 
     @session = Stripe::Checkout::Session.create(
-      # went to stripe API, looked up sessions, figured it all out..
-      payment_method_types: ['card'],
-      mode: 'payment',
-      success_url: checkout_success_url + "?session_id={checkout_session_id}",
-      cancel_url: checkout_cancel_url,
-      line_items: items #+ [
+      payment_method_types: ["card"],
+      mode:                 "payment",
+      success_url:          root_url,
+      cancel_url:           root_url,
+      line_items:           items # + [
       #   {
       #     name: 'GST',
       #     description: 'Goods and Services Tax',
@@ -50,10 +45,9 @@ class CheckoutController < ApplicationController
   end
 
   def success
-    @session - Stripe::Checkout::Session.retrieve(params[:session_id])
+    Stripe::Checkout::Session.retrieve(params[:session_id])
     @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
   end
 
-  def cancel
-  end
+  def cancel; end
 end
