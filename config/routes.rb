d8610e49c5ef5af75d1dc2213edcc447ef568a01
Foo Tang <godfoo@gmail.com>
2023-11-29 10:06:25 -0600

    Added an about and contact page.

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   resources :products
   resources :wrestlers
   resources :producttypes
+  resources :abouts
 end
---
+++ db/migrate/20231129160035_create_abouts.rb
@@ -0,0 +1,11 @@
+class CreateAbouts < ActiveRecord::Migration[7.0]
+  def change
+
+    create_table :abouts do |t|
+      t.text :contact
+      t.text :about
+
+      t.timestamps
+    end
+  end
+end
