fe3f59b476f760849e96f3e49db2d10395faa875
Chelsea Bahsler <chelseabahsler@gmail.com>
2023-12-04 10:33:32 -0600

    navbar using columns

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :initialize_session
   helper_method :cart
+  helper_method :categories
 
   private
 
   def initialize_session
     session[:shopping_cart] ||= [] # empty of product IDs
+  end
+
+  def categories
+    @producttypes = Producttype.all
   end
 
   def cart
